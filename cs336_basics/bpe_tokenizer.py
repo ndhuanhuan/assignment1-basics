63d05fd64b4a63b7fc59f27ef035428db19869b8
ndhuanhuan <ndhuanhuan@Dees-Mac-mini.local>
2025-07-27 22:28:59 -0700

    code clean up

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -1,77 +1,8 @@
 import os
 import regex as re
-from typing import BinaryIO
-from typing import Iterable, Iterator
 from collections import defaultdict
 from multiprocessing import Process, Queue
-import time
-import multiprocessing
-
-def find_chunk_boundaries(
-    file: BinaryIO, 
-    desired_num_chunks: int, 
-    split_special_token: bytes
-) -> list[int]:
-    """
-    Chunk the file into parts that can be counted independently.
-    May return fewer chunks if the boundaries end up overlapping.
-    """
-    assert isinstance(split_special_token, bytes), (
-        "Must represent special token as a bytestring"
-    )
-
-    # Get total file size in bytes
-    file.seek(0, os.SEEK_END)
-    file_size = file.tell()
-    file.seek(0)
-
-    chunk_size = file_size // desired_num_chunks
-
-    # Initial guesses for chunk boundary locations, uniformly spaced
-    # Chunks start on previous index, don't include last index
-    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
-    chunk_boundaries[-1] = file_size
-
-    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
-
-    for bi in range(1, len(chunk_boundaries) - 1):
-        initial_position = chunk_boundaries[bi]
-        file.seek(initial_position)  # Start at boundary guess
-        while True:
-            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
-
-            # If EOF, this boundary should be at the end of the file
-            if mini_chunk == b"":
-                chunk_boundaries[bi] = file_size
-                break
-
-            # Find the special token in the mini chunk
-            found_at = mini_chunk.find(split_special_token)
-            if found_at != -1:
-                chunk_boundaries[bi] = initial_position + found_at
-                break
-            initial_position += mini_chunk_size
-
-    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
-    return sorted(set(chunk_boundaries))
-
-def split_by_special_tokens(text: str, special_tokens: list[str]) -> list[str]:
-    """
-    Split on the special tokens
-    example: 
-        text = "Hello world! <|endoftext|> Great!" 
-        special_tokens = "<|endoftext|>"
-        result = ['Hello world! ', '<|endoftext|>', ' Great!']
-    """
-    special_tokens_sorted = sorted(special_tokens, key=lambda x: -len(x))
-    if not special_tokens_sorted:
-        parts = [text]
-    else:
-        pattern = "|".join(re.escape(tok) for tok in special_tokens_sorted)
-        parts = re.split('(' + pattern + ')', text)
-
-    return parts
-
+from cs336_basics.utils import find_chunk_boundaries, split_by_special_tokens
 
 class BPETokenizer():
 
