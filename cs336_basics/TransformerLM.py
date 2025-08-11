c5bae40c0a47ce672ba9b78cd83cbb3a31934d95
ndhuanhuan <ndhuanhuan@Dees-Mac-mini.local>
2025-08-10 18:30:11 -0700

    add more comments

--- cs336_basics/TransformerLM.py
+++ cs336_basics/TransformerLM.py
@@ -11,13 +11,14 @@ from cs336_basics.Transformer import TransformerBlock
 class TransformerLM(nn.Module):
     def __init__(
             self, 
-            vocab_size: int, 
-            context_length: int, 
-            d_model: int, 
-            num_layers: int, 
-            num_heads: int, 
-            d_ff: int,
-            rope_theta: float):
+            vocab_size: int,         # The number of unique items in the output vocabulary to be predicted.
+            context_length: int,     # The maximum number of tokens to process at once.
+            d_model: int,            # The dimensionality of the model embeddings and sublayer outputs.
+            num_layers: int,         # The number of Transformer layers to use.
+            num_heads: int,          # Number of heads to use in multi-headed attention. d_model must be evenly divisible by num_heads.
+            d_ff: int,               # Dimensionality of the feed-forward inner layer (section 3.3).
+            rope_theta: float        # The angle rate for rotary positional encoding.
+            ):
         super().__init__()
         self.vocab_size = vocab_size
         self.context_length = context_length
@@ -35,11 +36,21 @@ class TransformerLM(nn.Module):
             self, 
             in_indices: Int[Tensor, "batch_size sequence_length"]
             ) -> Float[Tensor, "batch_size sequence_length vocab_size"]:
-        x=self.token_embeddings(in_indices) #bs,sq_len,d_model
-
+        # in_indices: (batch_size, sequence_length)
+        # Output shape: (batch_size, sequence_length, d_model)
+        x = self.token_embeddings(in_indices)
+        
+        # x shape: (batch_size, sequence_length, d_model)
+        # Pass through each Transformer block
+        # output shape after each block: (batch_size, sequence_length, d_model)
         for layer in self.layers:
-            x=layer(x)
+            x = layer(x)
+
+        # x shape before is (batch_size, sequence_length, d_model)
+        # Apply final layer normalization and linear layer
+        # x shape after ln_final: (batch_size, sequence_length, d_model)
+        # logits shape: (batch_size, sequence_length, vocab_size)
         x = self.ln_final(x)
-        logits = self.lm_head(x) #bs seq_len vs
+        logits = self.lm_head(x)
 
         return logits
