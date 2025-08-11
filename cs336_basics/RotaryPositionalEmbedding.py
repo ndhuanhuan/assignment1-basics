03a6416605d7393787ef49a66ec5296bc1a1b267
ndhuanhuan <ndhuanhuan@Dees-Mac-mini.local>
2025-08-10 17:29:36 -0700

    more explain

--- cs336_basics/RotaryPositionalEmbedding.py
+++ cs336_basics/RotaryPositionalEmbedding.py
@@ -150,36 +150,3 @@ class RotaryPositionalEmbedding(nn.Module):
         out[..., ::2] = out_even
         out[..., 1::2] = out_odd
         return out
-
-    # def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
-    #     """
-    #     Apply rotary positional embedding to the input tensor.
-
-    #     Args:
-    #         x (torch.Tensor): Input tensor of shape (..., seq_len, d_k).
-    #         token_positions (torch.Tensor): Tensor of shape (..., seq_len) specifying token positions.
-
-    #     Returns:
-    #         torch.Tensor: Tensor of the same shape as x with RoPE
-    #     """
-    #     *batch_dims,seq_len,d_k = x.shape
-
-
-
-    #     assert d_k == self.d_k, " d_k must match"
-        
-    #     #make last dim into pairs
-    #     x_=x.view(*batch_dims, seq_len,d_k//2,2)
-    #     x1,x2=x_[...,0],x_[...,1]
-
-    #     cos=self.cos_cached[token_positions]
-    #     sin=self.sin_cached[token_positions]
-
-    #     #rotation
-    #     x_rotated_0=x1 * cos - x2 *sin 
-    #     x_rotated_1=x1*sin +  x2 * cos
-
-    #     x_rotated_stacked=torch.stack([x_rotated_0,x_rotated_1],dim=-1)
-    #     x_rotated_stacked=x_rotated_stacked.view(*batch_dims,seq_len,d_k)
-
-    #     return x_rotated_stacked
